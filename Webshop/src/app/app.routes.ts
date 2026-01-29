30a8dbde5ec496ee8d006551f321b70a0c970d05
hauszmanndavid <hauszmannd@gmail.com>
2026-01-29 10:10:23 +0100

    changes in routes

--- Webshop/src/app/app.routes.ts
+++ Webshop/src/app/app.routes.ts
@@ -1,17 +1,42 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './home-page/home-page';
-import { Login } from './login/login';
-import { Registration } from './registration/registration';
-import { Cart } from './cart/cart';
 
 export const routes: Routes = [
+  // Kezdőlap frissítéskor is
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-  { path: 'home', component: HomePage },
-  { path: 'login', component: Login },
-  { path: 'register', component: Registration },
-  { path: 'cart', component: Cart },
-  { path: 'checkout', loadComponent: () => import('./checkout/checkout').then(m => m.Checkout) },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home-page/home-page').then(m => m.HomePage),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./cart/cart').then(m => m.Cart),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./checkout/checkout').then(m => m.Checkout),
+  },
 
-  { path: '**', redirectTo: 'home' }
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login').then(m => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./registration/registration').then(m => m.Registration),
+  },
+
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./search-result/search-result').then(m => m.SearchResult),
+  },
+
+  // 404 fallback
+  { path: '**', redirectTo: 'home' },
 ];
